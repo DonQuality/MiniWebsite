24a39208456eba81766bb73cd7bb87a06dda743a
DonQuality <santosemmanuel@outlook.com>
2015-03-05 08:52:45 -0800

    Mini Website and Form

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var showForm = function(){
+  var registerForm = document.getElementById('register-form');
+  registerForm.style.visibility = "visible";
+};
+
+document.getElementById('show-R-Form').addEventListener('click', showForm, false);
